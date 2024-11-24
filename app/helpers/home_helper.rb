c4bee5fc02049b02bcde72d5d206163719696806
OlivierLeal <olivierleal61@gmail.com>
2024-11-24 18:28:40 -0300

    hotfix: corrige alguns erros e adiciona novo estilo as paginas

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+
+  def area_inicial
+    if current_user.cliente?
+      render partial: "home/inicial_cliente"
+    else
+      render partial: "home/inicial_tecnico"
+    end
+  end
 end
