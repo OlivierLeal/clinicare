7e5c161d887894bfb3719626dcf1792be623e943
OlivierLeal <olivierleal61@gmail.com>
2024-11-27 19:13:13 -0300

    adiciona nome e cnpj em usuarios

---
+++ db/migrate/20241127220223_add_cnpj_to_user.rb
@@ -0,0 +1,5 @@
+class AddCnpjToUser < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :cnpj, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_18_022823) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_27_220223) do
   create_table "calls", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_18_022823) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "group"
+    t.string "name"
+    t.string "cnpj"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
